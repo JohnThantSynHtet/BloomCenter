9e31f1674c8999043b8fbd5bc155a8da0607bcce
Htin <htinlwin@outlook.com>
2025-09-23 23:31:42 -0500

    lol

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing Testing 1234</Text>
+      <Text>FLOWER BOY</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffda03',
     alignItems: 'center',
     justifyContent: 'center',
   },
